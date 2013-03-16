6e5d8739d12a81020a1edcefd8f541ddb9ea02fd
Bhushan Shah <bhush94@gmail.com>
2013-03-16 22:04:59 +0530

    Now you can change your name and surname, I mean all info.

---
+++ test.php
@@ -0,0 +1,6 @@
+﻿<?php
+	echo "Welcome to sandboxed Testing console of Blogin.... <br>";
+	echo "This page will not provide UI so that you can easily test out your php codes <br>";
+	echo "Edit me and see change in output <br>";
+	exit;
+?>
